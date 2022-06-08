d86d72d17e78d812831690528795f5d1495bdf55
Renato Ruppel <heruppel2@gmail.com>
2022-06-07 21:11:11 -0300

    husky try

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,6 @@ import './index.css';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );

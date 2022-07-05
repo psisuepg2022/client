9f74e9b47a7947fdc94fd70b93b021adc35f5a97
Renato Ruppel <heruppel2@gmail.com>
2022-07-05 15:45:10 -0300

    feat(card-selector): added mock for card selector positioning

--- src/components/TopToolbar/index.tsx
+++ src/components/TopToolbar/index.tsx
@@ -24,6 +24,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { ToolbarProps, View } from 'react-big-calendar';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import CardSelector from '../CardSelector';
 
 type CustomToolbarProps = {
   onRangeChange: (range: Date[], view?: View) => void;
@@ -90,72 +91,75 @@ const TopToolbar = ({
   };
 
   return (
-    <Container>
-      <EarlyContent>
-        <ClinicTitle>KLINIK</ClinicTitle>
-      </EarlyContent>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Container>
+        <EarlyContent>
+          <ClinicTitle>KLINIK</ClinicTitle>
+        </EarlyContent>
 
-      <MiddleContent>
-        <IconButton onClick={goToBack}>
-          <AiOutlineLeft style={{ color: '#FFF', fontSize: 30 }} />
-        </IconButton>
-        {label()}
-        <IconButton onClick={goToNext}>
-          <AiOutlineRight style={{ color: '#FFF', fontSize: 30 }} />
-        </IconButton>
-      </MiddleContent>
+        <MiddleContent>
+          <IconButton onClick={goToBack}>
+            <AiOutlineLeft style={{ color: '#FFF', fontSize: 30 }} />
+          </IconButton>
+          {label()}
+          <IconButton onClick={goToNext}>
+            <AiOutlineRight style={{ color: '#FFF', fontSize: 30 }} />
+          </IconButton>
+        </MiddleContent>
 
-      <LatterContent>
-        <TodayButton onClick={goToCurrent} variant="outlined">
-          Hoje
-        </TodayButton>
+        <LatterContent>
+          <TodayButton onClick={goToCurrent} variant="outlined">
+            Hoje
+          </TodayButton>
 
-        <FormProvider {...formMethods}>
-          <FormControl>
-            <StyledInputLabel>Modo</StyledInputLabel>
-            <StyledSelect
-              name="mode"
-              label="Modo"
-              defaultValue={0}
-              onChange={(e: SelectChangeEvent<unknown>) =>
-                handleViewChange(e.target.value as number)
-              }
-              value={view === 'day' ? 0 : view === 'week' ? 1 : 2}
-            >
-              <StyledMenuItem value={0}>Dia</StyledMenuItem>
-              <StyledMenuItem value={1}>Semana</StyledMenuItem>
-              <StyledMenuItem value={2}>Mês</StyledMenuItem>
-            </StyledSelect>
-          </FormControl>
-        </FormProvider>
+          <FormProvider {...formMethods}>
+            <FormControl>
+              <StyledInputLabel>Modo</StyledInputLabel>
+              <StyledSelect
+                name="mode"
+                label="Modo"
+                defaultValue={0}
+                onChange={(e: SelectChangeEvent<unknown>) =>
+                  handleViewChange(e.target.value as number)
+                }
+                value={view === 'day' ? 0 : view === 'week' ? 1 : 2}
+              >
+                <StyledMenuItem value={0}>Dia</StyledMenuItem>
+                <StyledMenuItem value={1}>Semana</StyledMenuItem>
+                <StyledMenuItem value={2}>Mês</StyledMenuItem>
+              </StyledSelect>
+            </FormControl>
+          </FormProvider>
 
-        <IconButton
-          id="basic-button"
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
-            open ? handleClose() : handleClick(e)
-          }
-        >
-          <AiOutlineUser style={{ fontSize: 40, color: '#FFF' }} />
-        </IconButton>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-          sx={{ zIndex: 999 }}
-        >
-          <MenuItem>Perfil</MenuItem>
-          <hr />
-          <MenuItem>Logout</MenuItem>
-        </Menu>
-      </LatterContent>
-    </Container>
+          <IconButton
+            id="basic-button"
+            aria-controls={open ? 'basic-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              open ? handleClose() : handleClick(e)
+            }
+          >
+            <AiOutlineUser style={{ fontSize: 40, color: '#FFF' }} />
+          </IconButton>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              'aria-labelledby': 'basic-button',
+            }}
+            sx={{ zIndex: 999 }}
+          >
+            <MenuItem>Perfil</MenuItem>
+            <hr />
+            <MenuItem>Logout</MenuItem>
+          </Menu>
+        </LatterContent>
+      </Container>
+      <CardSelector /> {/* MOCK ONLY */}
+    </div>
   );
 };
 

a20dba6cbc1d02c1eea1aff03b37d5533f8784bb
Renato Ruppel <heruppel2@gmail.com>
2022-07-28 22:25:58 -0300

    refactor(profile): reworked profile inputs with validation

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { IconButton, Typography } from '@mui/material';
 import {
   Box,
   ButtonContainer,
@@ -11,19 +11,35 @@ import {
   PersonalInfoHalf,
   StyledButton,
 } from './styles';
-import { AiOutlineLeft } from 'react-icons/ai';
+import { FiChevronLeft } from 'react-icons/fi';
 import { colors } from '../../global/colors';
-import { FormProvider, useForm } from 'react-hook-form';
+import { FieldValues, FormProvider, useForm } from 'react-hook-form';
 import ControlledDatePicker from '../../components/ControlledDatePicker';
 import ControlledInput from '../../components/ControlledInput';
 import SectionDivider from '../../components/SectionDivider';
+import { useNavigate } from 'react-router-dom';
+import { isAfter } from 'date-fns';
+
+type ProfileFormProps = {
+  clinic: {
+    name: string;
+    email: string;
+  };
+  name: string;
+  email?: string;
+  contact_number?: string;
+  birthdate: string;
+  CPF: string;
+};
 
 const Profile = (): JSX.Element => {
   const formMethods = useForm();
   const { handleSubmit } = formMethods;
+  const navigate = useNavigate();
 
-  const onSubmit = (data: any): void => {
-    console.log('DATA', data);
+  const onSubmit = (data: FieldValues): void => {
+    const formData: ProfileFormProps = data as ProfileFormProps;
+    console.log('DATA', formData);
   };
 
   return (
@@ -31,7 +47,11 @@ const Profile = (): JSX.Element => {
       <Box>
         <Content>
           <Header>
-            <AiOutlineLeft style={{ color: colors.TEXT, fontSize: '2.5rem' }} />
+            <IconButton onClick={() => navigate('/agenda')}>
+              <FiChevronLeft
+                style={{ color: colors.TEXT, fontSize: '2.5rem' }}
+              />
+            </IconButton>
             <Typography fontSize={'2.5rem'}>Perfil da Clínica</Typography>
           </Header>
 
@@ -41,15 +61,26 @@ const Profile = (): JSX.Element => {
 
               <PersonalInfo>
                 <ControlledInput
-                  name="clinicName"
+                  name="clinic.name"
                   label="Nome"
-                  required
                   defaultValue="KLINIK"
+                  rules={{
+                    required: {
+                      value: true,
+                      message: 'O nome da clínica é obrigatório',
+                    },
+                  }}
                 />
                 <ControlledInput
-                  name="clinicEmail"
+                  name="clinic.email"
                   label="Email"
                   defaultValue="klinik@email.com"
+                  rules={{
+                    required: {
+                      value: true,
+                      message: 'O email da clínica é obrigatório',
+                    },
+                  }}
                 />
               </PersonalInfo>
 
@@ -59,8 +90,13 @@ const Profile = (): JSX.Element => {
                 <ControlledInput
                   name="name"
                   label="Nome"
-                  required
                   defaultValue="Peter Doppler"
+                  rules={{
+                    required: {
+                      value: true,
+                      message: 'O nome do responsável é obrigatório',
+                    },
+                  }}
                 />
                 <ControlledInput
                   name="email"
@@ -72,8 +108,7 @@ const Profile = (): JSX.Element => {
                   <ControlledInput
                     name="cpf"
                     label="CPF"
-                    required
-                    defaultValue="00000000000"
+                    defaultValue="000.000.000-00"
                     mask={(s: string): string =>
                       `${s
                         .replace(/\D/g, '')
@@ -82,10 +117,35 @@ const Profile = (): JSX.Element => {
                         .replace(/(\d{3})(\d)/, '$1-$2')
                         .replace(/(-\d{2})\d+?$/, '$1')}`
                     }
+                    rules={{
+                      required: {
+                        value: true,
+                        message: 'O CPF do responsável é obrigatório',
+                      },
+                      minLength: {
+                        value: 14,
+                        message: 'Insira um CPF válido',
+                      },
+                      maxLength: {
+                        value: 14,
+                        message: 'Insira um CPF válido',
+                      },
+                    }}
                   />
                   <ControlledDatePicker
-                    name="birthday"
+                    name="birthdate"
                     label="Data de nascimento"
+                    defaultValue={new Date()}
+                    rules={{
+                      required: {
+                        value: true,
+                        message:
+                          'A data de nascimento do responsável é obrigatória',
+                      },
+                      validate: (date) =>
+                        !isAfter(date, new Date()) ||
+                        'A Data escolhida não pode ser superior à data atual',
+                    }}
                   />
                 </PersonalInfoHalf>
 
@@ -93,7 +153,6 @@ const Profile = (): JSX.Element => {
                   <ControlledInput
                     name="phone"
                     label="Telefone"
-                    required
                     defaultValue="00000000000"
                     mask={(s: string): string =>
                       `${s

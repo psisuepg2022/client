1b86089f79ae525df77e5460f289d56f52d378fa
Renato Ruppel <heruppel2@gmail.com>
2022-08-05 19:45:12 -0300

    feat(patients): added patients provider on routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import App from '../App';
 import SideBar from '../components/SideBar';
 import { AuthProvider } from '../contexts/Auth';
+import { PatientsProvider } from '../contexts/Patients';
 import Agenda from '../pages/Agenda';
 import Login from '../pages/Login';
 import Patients from '../pages/Patients';
@@ -12,89 +13,91 @@ import Profile from '../pages/Profile';
 const AppRoutes = () => {
   return (
     <AuthProvider>
-      <Routes>
-        {/* PRE-AUTH */}
-        <Route>
+      <PatientsProvider>
+        <Routes>
+          {/* PRE-AUTH */}
           <Route>
-            <Route path="/login" element={<Login />} />
+            <Route>
+              <Route path="/login" element={<Login />} />
+            </Route>
           </Route>
-        </Route>
 
-        {/* DASHBOARD */}
-        <Route>
-          <Route
-            path="/agenda"
-            element={
-              <div style={{ display: 'flex', overflow: 'hidden' }}>
-                <SideBar />
-                <Agenda />
-              </div>
-            }
-          />
-          <Route
-            path="/professionals"
-            element={
-              <div style={{ display: 'flex', overflow: 'hidden' }}>
-                <SideBar />
-                <App />
-              </div>
-            }
-          />
-          <Route
-            path="/collaborators"
-            element={
-              <div style={{ display: 'flex', overflow: 'hidden' }}>
-                <SideBar />
-                <App />
-              </div>
-            }
-          />
-          {/* PATIENTS PAGES */}
-          <Route
-            path="/patients"
-            element={
-              <div style={{ display: 'flex', overflow: 'hidden' }}>
-                <SideBar />
-                <Patients />
-              </div>
-            }
-          />
-          <Route
-            path="/patients/form"
-            element={
-              <div
-                style={{
-                  display: 'flex',
-                  overflow: 'hidden',
-                }}
-              >
-                <SideBar />
-                <PatientsForm />
-              </div>
-            }
-          />
-          <Route
-            path="/patients/form/:id"
-            element={
-              <div
-                style={{
-                  display: 'flex',
-                  overflow: 'hidden',
-                }}
-              >
-                <SideBar />
-                <PatientsForm />
-              </div>
-            }
-          />
-        </Route>
+          {/* DASHBOARD */}
+          <Route>
+            <Route
+              path="/agenda"
+              element={
+                <div style={{ display: 'flex', overflow: 'hidden' }}>
+                  <SideBar />
+                  <Agenda />
+                </div>
+              }
+            />
+            <Route
+              path="/professionals"
+              element={
+                <div style={{ display: 'flex', overflow: 'hidden' }}>
+                  <SideBar />
+                  <App />
+                </div>
+              }
+            />
+            <Route
+              path="/collaborators"
+              element={
+                <div style={{ display: 'flex', overflow: 'hidden' }}>
+                  <SideBar />
+                  <App />
+                </div>
+              }
+            />
+            {/* PATIENTS PAGES */}
+            <Route
+              path="/patients"
+              element={
+                <div style={{ display: 'flex', overflow: 'hidden' }}>
+                  <SideBar />
+                  <Patients />
+                </div>
+              }
+            />
+            <Route
+              path="/patients/form"
+              element={
+                <div
+                  style={{
+                    display: 'flex',
+                    overflow: 'hidden',
+                  }}
+                >
+                  <SideBar />
+                  <PatientsForm />
+                </div>
+              }
+            />
+            <Route
+              path="/patients/form/:id"
+              element={
+                <div
+                  style={{
+                    display: 'flex',
+                    overflow: 'hidden',
+                  }}
+                >
+                  <SideBar />
+                  <PatientsForm />
+                </div>
+              }
+            />
+          </Route>
 
-        {/* PROFILE */}
-        <Route path="/profile" element={<Profile />} />
+          {/* PROFILE */}
+          <Route path="/profile" element={<Profile />} />
 
-        {/* REDIRECT */}
-        <Route path="*" element={<Navigate to="/agenda" replace />} />
-      </Routes>
+          {/* REDIRECT */}
+          <Route path="*" element={<Navigate to="/agenda" replace />} />
+        </Routes>
+      </PatientsProvider>
     </AuthProvider>
   );
 };

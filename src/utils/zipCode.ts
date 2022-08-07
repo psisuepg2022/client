5641ebba07bafffc710ea1d451cc3e52236aee88
Renato Ruppel <heruppel2@gmail.com>
2022-08-06 22:35:15 -0300

    refactor(zipcode): removed try catch from inner zip code function

--- src/utils/zipCode.ts
+++ src/utils/zipCode.ts
@@ -2,19 +2,12 @@ import { api } from '../service';
 import { CepInfos } from '../interfaces';
 
 export const searchForCep = async (value: string): Promise<CepInfos | void> => {
-  try {
-    const treatedCep = value.replace(/\D/g, '');
-    const { data } = await api.get(
-      `https://viacep.com.br/ws/${treatedCep}/json`
-    );
+  const treatedCep = value.replace(/\D/g, '');
+  const { data } = await api.get(`https://viacep.com.br/ws/${treatedCep}/json`);
 
-    if (data.erro) {
-      throw new Error('CEP inválido');
-    }
-
-    return data as CepInfos;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    console.log('err', err);
+  if (data.erro) {
+    throw new Error('CEP inválido');
   }
+
+  return data as CepInfos;
 };

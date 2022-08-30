9ffb62d712106655e46c293025aa36c4b23c3d58
Renato Ruppel <heruppel2@gmail.com>
2022-08-30 16:25:09 -0300

    refactor(api): added local api for tests

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  //baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  baseURL: 'http://localhost:3333/',
 });

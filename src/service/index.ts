193466e31842b0d3398c9a0ca96c82e2ee169acb
Renato Ruppel <heruppel2@gmail.com>
2022-08-31 23:38:26 -0300

    fix(server): replaced local server

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  //baseURL: `${import.meta.env.VITE_API_URL}/api/`,
-  baseURL: 'http://localhost:3333/',
+  baseURL: `${import.meta.env.VITE_API_URL}/api/`,
+  //baseURL: 'http://localhost:3333/',
 });

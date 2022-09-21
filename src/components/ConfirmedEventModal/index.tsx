123dbcb845516a5e7105f42dcbdc324bc3ebf42c
Renato Ruppel <heruppel2@gmail.com>
2022-09-21 19:22:36 -0300

    feat(confirmed-event): initialized confirmed event modal component

---
+++ src/components/ConfirmedEventModal/index.tsx
@@ -0,0 +1,183 @@
+import React, { useState } from 'react';
+import {
+  AdditionalInfos,
+  Body,
+  ButtonsContainer,
+  EventPrimaryText,
+  Header,
+  ScheduleAtDate,
+  ScheduleAtText,
+  ScheduledAtContainer,
+  StatusText,
+  StyledBox,
+  StyledCancelButton,
+  StyledConfirmButton,
+  StyledModal,
+} from './styles';
+import { MdOutlineClose, MdOutlineStickyNote2 } from 'react-icons/md';
+import { AiFillSchedule } from 'react-icons/ai';
+import { colors } from '@global/colors';
+import { CircularProgress, IconButton } from '@mui/material';
+import { format } from 'date-fns';
+import { Event } from 'react-big-calendar';
+import { useSchedule } from '@contexts/Schedule';
+import {
+  idFromResource,
+  statusFromResource,
+  updatedAtFromResource,
+} from '@utils/schedule';
+import { showAlert } from '@utils/showAlert';
+import { dateFormat } from '@utils/dateFormat';
+
+type ConfirmedEventModalProps = {
+  open: boolean;
+  handleClose: (reason: 'backdropClick' | 'escapeKeyDown' | '') => void;
+  eventInfo: Event | undefined;
+};
+
+const ConfirmedEventModal = ({
+  open,
+  handleClose,
+  eventInfo,
+}: ConfirmedEventModalProps): JSX.Element => {
+  const { updateAppointmentStatus, setEvents } = useSchedule();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  if (!eventInfo) return <></>;
+
+  if (eventInfo && !eventInfo.title) return <></>;
+
+  const closeAll = (reason: 'backdropClick' | 'escapeKeyDown' | ''): void => {
+    handleClose(reason);
+  };
+
+  const updateStatus = async (status: string) => {
+    try {
+      setLoading(true);
+      const appointmentId = idFromResource(eventInfo.resource);
+      const { content, message } = await updateAppointmentStatus(
+        appointmentId,
+        status
+      );
+
+      if (!content) {
+        showAlert({
+          icon: 'error',
+          text: 'Ocorreu um problema ao atualizar a consulta',
+        });
+      }
+
+      setEvents((prev) => {
+        const newEvents: Event[] = prev.map((event) =>
+          idFromResource(event.resource) === content?.id
+            ? {
+                ...event,
+                resource: `${content?.resource}/${content?.id}/${content?.updatedAt}`,
+              }
+            : event
+        );
+
+        return newEvents;
+      });
+
+      closeAll('');
+
+      showAlert({
+        title: 'Sucesso!',
+        icon: 'success',
+        text: message,
+      });
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (e: any) {
+      showAlert({
+        icon: 'error',
+        text:
+          e?.response?.data?.message ||
+          'Ocorreu um problema ao atualizar a consulta',
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const updatedAtDisplay = (): string => {
+    const updateTime = updatedAtFromResource(eventInfo.resource)
+      .split('T')[1]
+      .substring(0, 5);
+    const updateDate = new Date(updatedAtFromResource(eventInfo.resource));
+    updateDate.setHours(Number(updateTime.split(':')[0]));
+    updateDate.setMinutes(Number(updateTime.split(':')[1]));
+
+    return dateFormat({
+      date: updateDate,
+      // eslint-disable-next-line quotes
+      stringFormat: "d 'de' MMMM 'de' yyyy 'às' HH:mm",
+    });
+  };
+
+  return (
+    <StyledModal
+      open={open}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onClose={(event: any, reason: 'backdropClick' | 'escapeKeyDown') =>
+        closeAll(reason)
+      }
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <StyledBox>
+        <Header>
+          <IconButton disabled={loading}>
+            <MdOutlineStickyNote2
+              style={{ fontSize: 35, color: colors.PRIMARY }}
+            />
+          </IconButton>
+          <StatusText>
+            Situação: <span>{statusFromResource(eventInfo.resource)}</span>
+          </StatusText>
+          <IconButton disabled={loading} onClick={() => closeAll('')}>
+            <MdOutlineClose style={{ fontSize: 35, color: colors.PRIMARY }} />
+          </IconButton>
+        </Header>
+        <Body>
+          <EventPrimaryText>{eventInfo.title}</EventPrimaryText>
+          <EventPrimaryText>
+            {format(eventInfo.start as Date, 'hh:mm')} -{' '}
+            {format(eventInfo.end as Date, 'hh:mm')}
+          </EventPrimaryText>
+
+          <AdditionalInfos>
+            <AiFillSchedule style={{ fontSize: 70, color: colors.PRIMARY }} />
+
+            <ScheduledAtContainer>
+              <ScheduleAtText>Confirmado em:</ScheduleAtText>
+              <ScheduleAtDate>{updatedAtDisplay()}</ScheduleAtDate>
+            </ScheduledAtContainer>
+          </AdditionalInfos>
+
+          <ButtonsContainer>
+            <StyledConfirmButton
+              disabled={loading}
+              onClick={() => updateStatus('4')}
+            >
+              {loading ? (
+                <CircularProgress style={{ color: '#FFF' }} size={20} />
+              ) : (
+                'CONCLUIR'
+              )}
+            </StyledConfirmButton>
+            <StyledCancelButton
+              disabled={loading}
+              onClick={() => updateStatus('5')}
+            >
+              AUSÊNCIA
+            </StyledCancelButton>
+          </ButtonsContainer>
+        </Body>
+      </StyledBox>
+    </StyledModal>
+  );
+};
+
+export default React.memo(ConfirmedEventModal);
---
+++ src/components/ConfirmedEventModal/styles.ts
@@ -0,0 +1,119 @@
+import { Box, Button, Modal, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import { colors } from '@global/colors';
+
+export const StyledModal = styled(Modal)`
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledBox = styled(Box)`
+  width: 25%;
+  height: auto;
+  background-color: #fff;
+  outline: none;
+  border-radius: 5px;
+  padding: 1rem;
+
+  @media (max-width: 1700px) {
+    width: 30%;
+  }
+  @media (max-width: 1500px) {
+    width: 40%;
+  }
+  @media (max-width: 1060px) {
+    width: 55%;
+  }
+  @media (max-width: 880px) {
+    width: 65%;
+  }
+`;
+
+export const Header = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Body = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 0 1rem;
+`;
+
+export const StatusText = styled(Typography)`
+  color: ${colors.TEXT};
+  font-size: 1.2rem;
+
+  & span {
+    color: ${colors.PRIMARY};
+    font-weight: 600;
+  }
+`;
+
+export const EventPrimaryText = styled(Typography)`
+  font-size: 1.5rem;
+  color: ${colors.TEXT};
+  font-weight: 600;
+`;
+
+export const AdditionalInfos = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: 4rem;
+`;
+
+export const ScheduledAtContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ScheduleAtText = styled('div')`
+  color: ${colors.TEXT};
+`;
+
+export const ScheduleAtDate = styled('div')`
+  font-weight: 500;
+`;
+
+export const ButtonsContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  width: 80%;
+  gap: 1rem;
+`;
+
+export const StyledConfirmButton = styled(Button)`
+  background-color: ${colors.PRIMARY};
+  color: #fff;
+  transition: 300ms;
+  width: 100%;
+  font-weight: 400;
+  font-size: 1rem;
+  height: 50px;
+
+  :hover {
+    background-color: ${colors.SECONDARY};
+  }
+`;
+
+export const StyledCancelButton = styled(Button)`
+  background-color: ${colors.DANGER};
+  color: #fff;
+  transition: 300ms;
+  width: 100%;
+  font-weight: 400;
+  font-size: 1rem;
+  height: 50px;
+
+  :hover {
+    background-color: ${colors.DANGER_HOVER};
+  }
+`;

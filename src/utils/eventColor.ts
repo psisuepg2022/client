26039a7fcec560034955513010eb36c134aec4cf
Renato Ruppel <heruppel2@gmail.com>
2022-09-21 19:33:45 -0300

    feat(status): granted new colors for cancelled and absence statuses

--- src/utils/eventColor.ts
+++ src/utils/eventColor.ts
@@ -3,10 +3,10 @@ import { EventStatus } from '@interfaces/EventStatus';
 
 export const eventColor = (status: keyof typeof EventStatus): string => {
   if (status === 'Agendado') return colors.SCHEDULED;
-  if (status === 'Cancelado') return colors.DANGER;
+  if (status === 'Cancelado') return colors.CANCELLED;
   if (status === 'Confirmado') return colors.CONFIRMED;
   if (status === 'Concluído') return colors.CONCLUDED;
-  if (status === 'Ausência') return colors.DANGER_HOVER;
+  if (status === 'Ausência') return colors.ABSENCE;
 
   return colors.SCHEDULED;
 };

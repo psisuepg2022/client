c8f620d7f470f78ce47f5b80e10746b73c8a880f
HERuppel <heruppel2@gmail.com>
2022-09-24 13:11:36 -0300

    feat(owner): initialized owner context

---
+++ src/contexts/Owner/index.tsx
@@ -0,0 +1,44 @@
+import React, { createContext, useContext } from 'react';
+import { Response } from '@interfaces/Response';
+import { api } from '@service/index';
+import { Owner } from '@models/Owner';
+
+type OwnerContextData = {
+  getProfile: () => Promise<Response<Owner>>;
+};
+
+type OwnerProviderProps = {
+  children: React.ReactElement;
+};
+
+const OwnerContext = createContext<OwnerContextData>({} as OwnerContextData);
+
+export const OwnerProvider: React.FC<OwnerProviderProps> = ({
+  children,
+}: OwnerProviderProps) => {
+  const getProfile = async (): Promise<Response<Owner>> => {
+    const { data }: { data: Response<Owner> } = await api.get('owner/profile');
+
+    return data;
+  };
+
+  return (
+    <OwnerContext.Provider
+      value={{
+        getProfile,
+      }}
+    >
+      {children}
+    </OwnerContext.Provider>
+  );
+};
+
+export const useOwner = (): OwnerContextData => {
+  const context = useContext(OwnerContext);
+
+  if (!context) {
+    throw new Error('Este hook deve ser utilizado dentro de seu provider');
+  }
+
+  return context;
+};

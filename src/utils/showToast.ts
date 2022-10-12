dd242e2727918cb7cf7e320c24102d7526b5c999
Renato Ruppel <heruppel2@gmail.com>
2022-10-12 15:49:52 -0300

    feat(snackbar): created snackbar for success messages

---
+++ src/utils/showToast.ts
@@ -0,0 +1,34 @@
+import Swal, { SweetAlertIcon, SweetAlertOptions } from 'sweetalert2';
+import './custom.css';
+
+export type AlertProps = {
+  icon?: SweetAlertIcon;
+  text: string;
+  // customClass?: string;
+} & SweetAlertOptions;
+
+export const showToast = ({
+  text,
+  icon,
+  // customClass,
+  ...rest
+}: AlertProps) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'bottom-left',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+    showCloseButton: true,
+  });
+
+  return Toast.fire({
+    icon: icon || 'success',
+    title: text,
+    ...rest,
+  });
+};

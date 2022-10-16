5bc13bfd851d3f054e5948948551fc0e536fab20
Renato Ruppel <heruppel2@gmail.com>
2022-10-16 16:25:44 -0300

    style(sidebar): min width

--- src/components/SideBar/styles.ts
+++ src/components/SideBar/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled('div')`
   width: 250px;
   min-height: 100vh;
   max-width: 250px;
+  min-width: 250px;
   display: flex;
   flex-direction: column;
   align-items: center;

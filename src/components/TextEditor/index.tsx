190acf77981819fac8ab663ac9572da38128f84a
Renato Ruppel <heruppel2@gmail.com>
2022-10-17 22:00:32 -0300

    feat(quill): new editor with new lib

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useQuill } from 'react-quilljs';
+import React, { useState } from 'react';
 import 'quill/dist/quill.snow.css';
 import { Container, Content, StyledButton } from './style';
 import { CircularProgress } from '@mui/material';
+import ReactQuill from 'react-quill';
 
 type TextEditorProps = {
   saveComment?: (text: string) => Promise<void>;
@@ -15,40 +15,15 @@ const TextEditor = ({
   comment,
   readOnly,
 }: TextEditorProps): JSX.Element => {
-  const { quill, quillRef } = useQuill({
-    modules: {
-      toolbar: '#toolbar',
-    },
-    readOnly,
-  });
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [value, setValue] = useState<string>(comment ? comment : '');
 
-  useEffect(() => {
-    if (comment && quill) {
-      quill.root.innerHTML = comment;
-      setLoading(false);
-    }
-    if (!comment) {
-      setLoading(false);
-    }
-  }, [quill]);
-
-  useEffect(() => {
-    if (quill) {
-      if (readOnly) {
-        quill.disable();
-      } else {
-        quill.enable();
-      }
-    }
-  }, [readOnly]);
+  console.log('CONSOLE', comment);
 
   const onSubmit = async (): Promise<void> => {
-    const data = quill?.root.innerHTML;
-
     if (saveComment) {
       setLoading(true);
-      await saveComment((data === '<p><br></p>' ? '' : data) || '');
+      await saveComment((value === '<p><br></p>' ? '' : value) || '');
       setLoading(false);
     }
   };
@@ -56,46 +31,65 @@ const TextEditor = ({
   return (
     <Container>
       <Content>
-        <div id="toolbar" style={{ ...(readOnly && { display: 'none' }) }}>
-          <select
-            className="ql-size"
-            defaultValue="medium"
-            style={{ marginLeft: 10 }}
-          >
-            <option value="small">Pequena</option>
-            <option value="medium">Média</option>
-            <option value="large">Grande</option>
-            <option value="huge">Enorme</option>
-          </select>
-          <span
-            className="ql-formats"
-            style={{ marginLeft: 10, marginRight: 10 }}
-          >
-            <button className="ql-bold" />
-            <button className="ql-italic" />
-            <button className="ql-underline" />
-            <button className="ql-strike" />
-          </span>
-          <span className="ql-formats">
-            <button className="ql-list" value="ordered" />
-            <button className="ql-list" value="bullet" />
-          </span>
-          <span className="ql-formats">
-            <select style={{ marginLeft: 10 }} className="ql-align" />
-            <select style={{ marginLeft: 10 }} className="ql-color" />
-            <select className="ql-background" />
-          </span>
+        <div>
+          <div id="toolbar">
+            <select
+              className="ql-size"
+              defaultValue="medium"
+              style={{ marginLeft: 10 }}
+              disabled={readOnly}
+            >
+              <option value="small">Pequena</option>
+              <option value="medium">Média</option>
+              <option value="large">Grande</option>
+              <option value="huge">Enorme</option>
+            </select>
+            <span
+              className="ql-formats"
+              style={{ marginLeft: 10, marginRight: 10 }}
+            >
+              <button disabled={readOnly} className="ql-bold" />
+              <button disabled={readOnly} className="ql-italic" />
+              <button disabled={readOnly} className="ql-underline" />
+              <button disabled={readOnly} className="ql-strike" />
+            </span>
+            <span className="ql-formats">
+              <button disabled={readOnly} className="ql-list" value="ordered" />
+              <button disabled={readOnly} className="ql-list" value="bullet" />
+            </span>
+            <span className="ql-formats">
+              <select
+                disabled={readOnly}
+                style={{ marginLeft: 10 }}
+                className="ql-align"
+              />
+              <select
+                disabled={readOnly}
+                style={{ marginLeft: 10 }}
+                className="ql-color"
+              />
+              <select disabled={readOnly} className="ql-background" />
+            </span>
+          </div>
+          <ReactQuill
+            style={{
+              height: '80%',
+              maxHeight: '70%',
+              width: '100%',
+              fontFamily: 'Poppins',
+              fontSize: '1.2rem',
+            }}
+            modules={{
+              toolbar: {
+                container: '#toolbar',
+              },
+            }}
+            value={value}
+            onChange={setValue}
+            readOnly={readOnly}
+          />
         </div>
-        <div
-          style={{
-            height: '80%',
-            maxHeight: '70%',
-            width: '100%',
-            fontFamily: 'Poppins',
-            fontSize: '1.2rem',
-          }}
-          ref={quillRef}
-        />
+
         <StyledButton disabled={loading || readOnly} onClick={onSubmit}>
           {loading ? (
             <CircularProgress style={{ color: '#FFF' }} size={20} />

58c5f99e9deb048adc561eee1f2f4a56f6d1e425
Renato Ruppel <heruppel2@gmail.com>
2022-11-04 00:22:39 -0300

    refactor(employee): reworked filter to one input only

--- src/contexts/Employees/index.tsx
+++ src/contexts/Employees/index.tsx
@@ -1,14 +1,13 @@
 import React, { createContext, useContext, useState } from 'react';
 import { Response } from '@interfaces/Response';
 import { api } from '@service/index';
-import { SearchFilter } from '@interfaces/SearchFilter';
 import { ItemList } from '@interfaces/ItemList';
 import { FormEmployee, Employee, UpdateEmployee } from '@models/Employee';
 
 type ListProps = {
   page?: number;
   size?: number;
-  filter?: SearchFilter;
+  composed?: string;
 };
 
 type EmployeesContextData = {
@@ -38,14 +37,14 @@ export const EmployeesProvider: React.FC<EmployeesProviderProps> = ({
   const list = async ({
     size,
     page,
-    filter,
+    composed,
   }: ListProps): Promise<Response<ItemList<Employee>>> => {
     const { data }: { data: Response<ItemList<Employee>> } = await api.post(
       (page as number) >= 0 && size
         ? `employee/search?page=${page}&size=${size}`
         : 'employee/search',
       {
-        ...filter,
+        composed,
       }
     );
 

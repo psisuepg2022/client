924959ca29e8a58c6a5804100a63430f710a321d
Renato Ruppel <heruppel2@gmail.com>
2022-11-15 22:58:11 -0300

    feat(help): added help for schedule actions

---
+++ src/components/ScheduleHelpModal/index.tsx
@@ -0,0 +1,168 @@
+import React from 'react';
+import {
+  Body,
+  EventPrimaryText,
+  Header,
+  ScheduleAtText,
+  StatusText,
+  StyledBox,
+  StyledModal,
+} from './styles';
+import { MdOutlineClose } from 'react-icons/md';
+import { colors } from '@global/colors';
+import { IconButton } from '@mui/material';
+import SectionDivider from '@components/SectionDivider';
+import { useAuth } from '@contexts/Auth';
+
+type ScheduleHelpModalProps = {
+  open: boolean;
+  handleClose: (reason: 'backdropClick' | 'escapeKeyDown' | '') => void;
+};
+
+const ScheduleHelpModal = ({
+  open,
+  handleClose,
+}: ScheduleHelpModalProps): JSX.Element => {
+  const {
+    user: { permissions },
+  } = useAuth();
+
+  const closeAll = (reason: 'backdropClick' | 'escapeKeyDown' | ''): void => {
+    handleClose(reason);
+  };
+
+  return (
+    <StyledModal
+      open={open}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      onClose={(event: any, reason: 'backdropClick' | 'escapeKeyDown') =>
+        closeAll(reason)
+      }
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <StyledBox>
+        <Header>
+          <MdOutlineClose style={{ fontSize: 35, color: 'transparent' }} />
+          <EventPrimaryText>Ações da agenda</EventPrimaryText>
+          <IconButton onClick={() => closeAll('')}>
+            <MdOutlineClose style={{ fontSize: 35, color: colors.PRIMARY }} />
+          </IconButton>
+        </Header>
+        <Body>
+          <div style={{ textIndent: '2rem' }}>
+            <StatusText>
+              {permissions.includes('USER_TYPE_EMPLOYEE')
+                ? `
+              As ações disponíveis na agenda são: Selecionar o profissional para agendamento, agendar uma consulta, confirmar, cancelar ou declarar ausência (após confirmação).
+              `
+                : permissions.includes('USER_TYPE_PROFESSIONAL')
+                ? `As ações disponíveis na agenda são: Agendar uma consulta (apenas para pacientes que já consultaram ao menos uma vez com o profissional),
+                concluir uma consulta e criar uma anotação para a consulta.
+                `
+                : 'Para o administrador, é possível apenas a visualização das agendas dos profissionais e seus eventos, e a navegação pelo calendário, não sendo possível realizar ações de agendamento.'}
+            </StatusText>
+          </div>
+
+          {permissions.includes('USER_TYPE_EMPLOYEE') ? (
+            <>
+              <SectionDivider>Seleção de profissional</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {`Os agendamentos podem ser feitos para todos os profissionais da clínica, portanto é necessário selecionar o profissional desejado na
+                  barra acima da agenda, clicando no nome para prosseguir com o agendamento para aquele profissional.
+                  `}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+
+          {permissions.includes('USER_TYPE_EMPLOYEE') ||
+          permissions.includes('USER_TYPE_PROFESSIONAL') ? (
+            <>
+              <SectionDivider>Agendamento</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {permissions.includes('USER_TYPE_EMPLOYEE')
+                    ? `
+                  Para realizar o agendamento de uma consulta, é necessário navegar para o dia pretendido utilizando a barra superior da agenda, no dia, clicar em um espaço da agenda disponível,
+                  na caixa de diálogo aberta, inserir ao menos três caracteres do nome do paciente e aguardar aproximadamente um segundo para a busca do paciente. Caso o paciente já esteja cadastrado,
+                  seu nome aparecerá na lista para seleção, é preciso clicar no nome do paciente para que as informações de confirmação sejam trazidas. Se confirmadas as informações, é necessário um 
+                  clique no botão "AGENDAR" para concluir o agendamento.
+                `
+                    : `
+                Para realizar o agendamento de uma consulta, é necessário navegar para o dia pretendido utilizando a barra superior da agenda, no dia, clicar em um espaço da agenda disponível,
+                na caixa de diálogo aberta, inserir ao menos três caracteres do nome do paciente e aguardar aproximadamente um segundo para a busca do paciente, para profissionais, o paciente 
+                em questão precisa ter se consultado com o profissional ao menos uma vez para que a busca tenha sucesso. Caso o paciente já esteja cadastrado,
+                seu nome aparecerá na lista para seleção, é preciso clicar no nome do paciente para que as informações de confirmação sejam trazidas. Se confirmadas as informações, é necessário um 
+                clique no botão "AGENDAR" para concluir o agendamento.
+                `}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+
+          {permissions.includes('USER_TYPE_EMPLOYEE') ? (
+            <>
+              <SectionDivider>Confirmação e cancelamento</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {`Quando a consulta estiver com status "Agendada", duas ações são liberadas: "CONFIRMAR" e "CANCELAR", ao confirmar a consulta mudará de status. Ao cancelar, se a data da consulta
+                    for futura, o evento será removido da agenda, liberando o espaço para reuso, caso a data seja passada, o evento permanecerá na agenda com o status de "Cancelada".`}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+
+          {permissions.includes('USER_TYPE_EMPLOYEE') ? (
+            <>
+              <SectionDivider>Ausência</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {`Quando a consulta está com status "Confirmada", o botão de "AUSÊNCIA" é liberado, para quando o paciente confirmar a consulta, mas mesmo assim não comparecer. 
+                  Caso a data da consulta seja futura, o evento será removido da agenda, liberando o espaço para reuso, caso a data seja passada, o evento permanecerá na agenda com o status de "Ausente".
+                  `}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+
+          {permissions.includes('USER_TYPE_PROFESSIONAL') ? (
+            <>
+              <SectionDivider>Horários da agenda</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {`Para o profissional, é possível alterar os horários e intervalos escolhidos na hora da configuração, no primeiro acesso. 
+                    Basta entrar na página de perfil, no ícone de usuário na barra superior, e na página do perfil clicar no ícone de relógio no canto superior direito.
+                    `}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+
+          {permissions.includes('USER_TYPE_PROFESSIONAL') ? (
+            <>
+              <SectionDivider>Bloqueios do profissional</SectionDivider>
+
+              <div style={{ textIndent: '2rem' }}>
+                <ScheduleAtText>
+                  {`O profissional também pode criar um bloqueio de horário para uma data específica. Para isso, basta selecionar um campo de horário disponível na agenda, 
+                  clicar no ícone do cadeado vermelho e escolher os horários de início e fim do bloqueio, estes estarão preenchidos inicialmente com os horários do campo
+                  selecionado na agenda, mas podem ser alterados, podendo ter a duração de um ou mais espaços de consulta na agenda.
+                    `}
+                </ScheduleAtText>
+              </div>
+            </>
+          ) : null}
+        </Body>
+      </StyledBox>
+    </StyledModal>
+  );
+};
+
+export default React.memo(ScheduleHelpModal);
---
+++ src/components/ScheduleHelpModal/styles.ts
@@ -0,0 +1,73 @@
+import { Box, Modal, Typography } from '@mui/material';
+import { styled } from '@mui/system';
+import { colors } from '@global/colors';
+
+export const StyledModal = styled(Modal)`
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledBox = styled(Box)`
+  width: 35%;
+  height: auto;
+  background-color: #fff;
+  outline: none;
+  border-radius: 5px;
+  padding: 1rem;
+
+  @media (max-width: 1700px) {
+    width: 40%;
+  }
+  @media (max-width: 1500px) {
+    width: 50%;
+  }
+  @media (max-width: 1060px) {
+    width: 55%;
+  }
+  @media (max-width: 880px) {
+    width: 65%;
+  }
+`;
+
+export const Header = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Body = styled('div')`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem 1rem;
+  max-height: 80vh;
+  overflow-y: scroll;
+`;
+
+export const StatusText = styled(Typography)`
+  color: ${colors.TEXT};
+  font-size: 1rem;
+
+  text-align: justify;
+
+  & span {
+    color: ${colors.PRIMARY};
+    font-weight: 600;
+  }
+`;
+
+export const EventPrimaryText = styled(Typography)`
+  font-size: 1.3rem;
+  text-align: justify;
+  color: ${colors.TEXT};
+  font-weight: 600;
+`;
+
+export const ScheduleAtText = styled('div')`
+  color: ${colors.TEXT};
+  text-align: justify;
+  font-size: 1rem;
+`;
